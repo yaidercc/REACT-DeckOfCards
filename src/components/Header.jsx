b0966853b5a6d2bbaf85644ac9e1ebab7c2d21ae
unknown <juan_molina23181@elpoli.edu.co>
2022-12-13 11:39:12 -0500

    se ha mejorado el diseño

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { TfiGame } from 'react-icons/tfi';
-
+// import { TfiGame } from 'react-icons/tfi';
+import {MdOutlineCasino} from 'react-icons/md'
 const Header = () => {
 	return (
 		<Navbar bg='dark' variant='dark'>
 			<Container>
 				<Navbar.Brand>
 					<div className='d-inline-block align-top '>
-						<TfiGame />
+						<MdOutlineCasino />
 					</div>{' '}
 					Deck Of Cards Game
 				</Navbar.Brand>
